99ce8d26d7ee257b4b900dd6c5a740bc0da28e3a
Tarun Dupati <tarunraju292000@gmail.com>
2025-06-18 12:45:50 +0530

    Add files via upload

---
+++ lab 1/script.js
@@ -0,0 +1,8 @@
+console.log('hai tarun')
+console.log('today i learn java script')
+console.log('🤐in heat')
+var x=1;
+console.log(a=x)
+// var x=1; not working only in upper word
+let b=5
+console.log(b=x)
